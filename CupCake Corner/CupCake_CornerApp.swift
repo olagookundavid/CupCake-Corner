cafef6606d5be39bcd5ab1f137714a770d5bb2f7
David Olagookun <erijesudo@gmail.com>
2023-03-16 00:00:18 +0100

    Initial Commit

---
+++ CupCake Corner/CupCake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCake_CornerApp.swift
+//  CupCake Corner
+//
+//  Created by David OH on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CupCake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
